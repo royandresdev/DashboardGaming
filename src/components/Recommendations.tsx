def7a54ed85438559549e18d78b72db7b0c551f6
royandresdev <royhuamanavila@gmail.com>
2025-07-30 00:51:25 -0500

    refactor: ajustar la clase del contenedor en el componente Recommendations para mejorar la adaptabilidad

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 interface Props { recommendations: string[] }
 const Recommendations: React.FC<Props> = ({ recommendations }) => (
-  <section className="h-full">
+  <section className="h-fit lg:h-full">
     <h2 className="text-lg font-semibold mb-4">Recomendados para vos</h2>
     <div className="bg-[#232733] rounded-xl shadow-md border border-gray-800 h-full flex items-center justify-center">
       {recommendations.map((rec, idx) => (
