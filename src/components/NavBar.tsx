d8ef3ffe9f77e1b06be7de1cd7d56ffa835a3fd0
royandresdev <royhuamanavila@gmail.com>
2025-08-01 02:28:14 -0500

    feat: agregar @iconify/react y mejorar la barra de navegación con nuevos enlaces

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,33 @@
+import { Icon } from "@iconify/react";
+
 const NavBar = () => (
-  <nav className="w-full bg-[#232733] py-4 px-8 flex flex-col gap-2 border-b border-gray-800">
-    <div className="text-center text-sm text-gray-400 tracking-wide">Nav bar</div>
+  <nav className="w-full py-4 px-8 flex flex-col gap-2">
+    <div className="max-w-7xl mx-auto flex items-center justify-between w-full">
+      <div className="flex items-center gap-2 text-xl">
+        <Icon icon="token:gamefi" className="text-5xl" /> <span>GameStats</span>
+      </div>
+      <div className="flex items-center gap-4">
+        <a
+          className="border block border-[#D732A8] bg-[#211C40] w-[189px] py-1 rounded-lg text-center"
+          href="#"
+        >
+          Dashboard
+        </a>
+        <a
+          className="border block border-transparent w-[189px] py-1 rounded-lg text-center"
+          href="#"
+        >
+          Mi biblioteca
+        </a>
+        <a
+          className="border block border-transparent w-[189px] py-1 rounded-lg text-center"
+          href="#"
+        >
+          Configuración
+        </a>
+      </div>
+      <div></div>
+    </div>
   </nav>
 );
 export default NavBar;
